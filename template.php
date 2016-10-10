22f1defdbe3c14f1c86d859ede897455de670e9d
DS <cckn1985@gmail.com>
2016-10-11 00:00:52 +0900

    day-off commit

---
+++ template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <h1>JavaScript</h1>
+  <script type="text/javascript">
+
+  </script>
+
+  <h2>PHP</h2>
+  <?php
+
+  ?>
+</body>
+</html>
